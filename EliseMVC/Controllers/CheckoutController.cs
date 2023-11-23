a7e0422c192412b41da43c8eddcaee85f34d929e
huyb11 <huynhhuy17112004@gmail.com>
2023-11-23 14:03:20 +0700

    checkout ui

---
+++ EliseMVC/Controllers/CheckoutController.cs
@@ -0,0 +1,17 @@
+﻿using EliseMVC.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EliseMVC.Controllers
+{
+    public class CheckoutController: Controller
+    {
+        DBEliseStoreEntitiess db = new DBEliseStoreEntitiess();
+
+        // GET: products
+        public ActionResult Cart()
+        {
+            return View(db.tblProducts.ToList());
+        }
+    }
+}
